118d43326e4ce3ed8051db099c4b5c34730e62d8
konstantin <borisovke@gmail.com>
2018-09-11 18:38:21 +0600

    The third assignment solution

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -1,62 +1,90 @@
 (function() {
 'use strict';
-angular.module('ShoppingListCheckOff', [])
-.controller('ToBuyController', ToBuyController)
-.controller('AlreadyBoughtController', AlreadyBoughtController)
-.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+angular.module('NarrowItDownApp', [])
+.controller('NarrowItDownController', NarrowItDownController)
+.service('MenuSearchService', MenuSearchService)
+.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com")
+.directive('foundItems', FoundItems)
+;
 
-ToBuyController.$inject = ['ShoppingListCheckOffService'];
-function ToBuyController(ShoppingListCheckOffService){
-    var toBuyList = this;
-    toBuyList.items = ShoppingListCheckOffService.getItemsToBuy();
-    toBuyList.buyItem = function (itemIndex) {
-      ShoppingListCheckOffService.buyItem(itemIndex);
-    };
+// Directive
+function FoundItems() {
+  var ddo = {
+    restrict: "E",
+    templateUrl: 'foundItems.html',
+    scope: {
+      foundItems: '<',
+      onRemove: '&'
+    },
+    controller: FoundItemsDirectiveController,
+    controllerAs: 'list',
+    bindToController: true
 
   };
 
-AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
-function AlreadyBoughtController(ShoppingListCheckOffService){
-    var boughtList = this;
-    boughtList.items = ShoppingListCheckOffService.getBoughtItems();
+  return ddo;
+}
+
+function FoundItemsDirectiveController() {
+   var list = this;
   };
 
-  function ShoppingListCheckOffService(){
-    var service = this;
-    var itemsToBuy = [];
-    var itemsBought = [];
+// The main controller
+NarrowItDownController.$inject = ['MenuSearchService'];
+function NarrowItDownController(MenuSearchService){
+    var menu = this;
+    menu.notFound = undefined;
+    menu.loadData = function(search)
+    {
+      menu.found = undefined;
+      if(search == undefined) search = "";
+      if(search == "") {
+        menu.notFound = true;
+      }
+      else {
+        var promise = MenuSearchService.getMatchedMenuItems(search);
+          promise.then(function (response) {
+          menu.found = response;
+          if(menu.found.length>0) menu.notFound = false;
+        }).catch(function (response) {
+         console.log("Something went terribly wrong.");
+        })
+      }
+    }
 
-    service.addItemToBuy = function (itemName, quantity) {
-      var item = {
-        name: itemName,
-        quantity: quantity
-      };
-      itemsToBuy.push(item);
+
+    menu.removeItem = function (itemIndex) {
+      this.found.splice(itemIndex, 1);
     };
+}
 
-    service.buyItem = function (itemIdex) {
-      var item = itemsToBuy[itemIdex];
-      console.log(item);
-      itemsToBuy.splice(itemIdex, 1);
-      itemsBought.push(item);
-      console.log(itemsBought);
-    };
+// The service
+MenuSearchService.$inject = ['$http', 'ApiBasePath', '$q'];
+function MenuSearchService($http, ApiBasePath, $q){
+  var service = this;
 
-    service.getItemsToBuy = function () {
-      return itemsToBuy;
-    };
+  service.getMatchedMenuItems = function (searchTerm) {
+    var response = $http({
+          method: "GET",
+          url: (ApiBasePath + "/menu_items.json")
+        });
+    if(searchTerm === undefined) searchTerm="";
+    var deffered = $q.defer();
+    response.then(function (response) {
+      var result = response.data.menu_items;
+      var found = new Array();
+      result.forEach(function(item){
+        if(item.description.toUpperCase().indexOf(searchTerm.toUpperCase()) !== -1){
+          found.push(item);
+        }
+      });
+      deffered.resolve(found);
+    })
+    .catch(function (response) {
+     deferred.reject(response);
+    })
+    return deffered.promise;
+};
 
-    service.getBoughtItems = function () {
-      return itemsBought;
-    };
-
-    // The initial array of items to buy
-    service.addItemToBuy('coockies', 10);
-    service.addItemToBuy('pills', 5);
-    service.addItemToBuy('apples', 10);
-    service.addItemToBuy('bottle of water', 10);
-    service.addItemToBuy('candies', 567);
-    service.addItemToBuy('carrot', 1);
-
-  }
+}
 })();
